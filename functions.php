a5f1dbfc4a5e21f9a1c68f1857affca66e6a495c
Matt Beall <mbeall@starverte.com>
2014-08-06 15:50:06 -0600

    Add function canvas_after_setup_theme

--- functions.php
+++ functions.php
@@ -4,3 +4,16 @@
  *
  * @package Flint/Canvas
  */
+
+if ( ! function_exists( 'canvas_after_setup_theme' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ */
+function canvas_after_setup_theme() {
+  require( get_stylesheet_directory() . '/inc/customizer.php' );
+  register_nav_menus( array(
+    'footer-left' => __( 'Footer (Left)', 'canvas' ),
+  ) );
+}
+endif; // canvas_after_setup_theme
+add_action( 'after_setup_theme', 'canvas_after_setup_theme', 10 );
